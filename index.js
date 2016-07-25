f4430a35455cb651ae35156f0a5c7cfbda0bbbd5
D-aVi-D <davids_mbox@bigmir.net>
2016-07-25 14:56:44 +0300

    Third commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import UserContainer from './components/userlist/usercontainer.js'
+import App from './containers/app'
+import About from './components/about'
+import Demo from './components/demo/demo.js'
+import Noname from './components/noname.js'
+import UserListContainer from './components/userlist/userlistcontainer.js'
 import {IndexRoute, Route, Router, browserHistory} from 'react-router';
 
 render(
-
-	{UserContainer}, document.getElementById('root')
+    (<Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={UserListContainer} />
+            <Route path="demo" component={Demo} />
+        </Route>
+    </Router>)
+    , document.getElementById('root')
 )
